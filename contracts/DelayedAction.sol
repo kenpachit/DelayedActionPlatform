ef26fd3abf5237f1c52e34b8fb98dcdbcb4f83b5
kenpachit <161153415+kenpachit@users.noreply.github.com>
2024-07-05 00:02:01 +0200

    advance the readability

--- contracts/DelayedAction.sol
+++ contracts/DelayedAction.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract DelayedActionPlatform {
@@ -16,31 +17,34 @@ contract DelayedActionPlatform {
     event ActionExecuted(uint256 indexed actionId, address indexed executor);
 
     /// @notice Creates a new delayed action
-    /// @param _executeAfter Number of seconds after which the action can be executed
+    /// @param _executeAfter Seconds to wait until the action can be executed
     /// @param _data Arbitrary data associated with the action
-    function createAction(uint256 _executeHeader, string memory _data) public {
-        require(_executeHeader > 0, "Execution delay must be greater than 0");
+    function createAction(uint256 _executeAfter, string memory _data) public {
+        require(_executeAfter > 0, "Execution delay must be > 0");
 
         uint256 actionId = nextActionId++;
+        uint256 executeAfter = block.timestamp + _executeAfter;
+
         actions[actionId] = Action({
             id: actionId, 
             creator: msg.sender, 
-            executeAfter: block.timestamp + _executeHeader, 
+            executeAfter: executeAfter, 
             executed: false, 
             data: _data
         });
 
-        emit ActionCreated(actionId, msg.sender, block.timestamp + _executeHeader, _data);
+        emit ActionCreated(actionId, msg.sender, executeAfter, _data);
     }
 
     /// @notice Executes a specified action, if conditions are met
     /// @param _actionId ID of the action to be executed
     function executeAction(uint256 _actionId) public {
-        Action storage action = actions[_actionId];
         require(_actionId < nextActionId, "Action does not exist");
+        Action storage action = actions[_actionId];
+
         require(block.timestamp >= action.executeAfter, "Action cannot be executed yet");
-        require(!action.executed, "Action has already been executed");
-        require(action.creator == msg.sender, "Only the action creator can execute it");
+        require(!action.executed, "Action already executed");
+        require(action.creator == msg.sender, "Only creator can execute");
 
         action.executed = true;
 
@@ -49,9 +53,9 @@ contract DelayedActionPlatform {
 
     /// @notice Fetches details of an action
     /// @param _actionId ID of the action to fetch
-    /// @return Action The action details
+    /// @return The action details
     function getAction(uint256 _actionId) public view returns (Action memory) {
-        require(_actionId < nextActionId, "Action does not exist");
+        require(_actionId < nextActionId, "Action does not work");
         return actions[_actionId];
     }
 }
